ddf2701d7f69f2f3041746c0aead6bb1d7e84965
ManuelBarreiro03 <manuelbarreiro2003@gmail.com>
2023-06-04 19:44:48 -0300

    casa

--- prySP1ManuelBarreiro/frmLogin.Designer.cs
+++ prySP1ManuelBarreiro/frmLogin.Designer.cs
@@ -113,6 +113,7 @@
             // 
             // lstModulo
             // 
+            this.lstModulo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.lstModulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstModulo.FormattingEnabled = true;
             this.lstModulo.Items.AddRange(new object[] {
